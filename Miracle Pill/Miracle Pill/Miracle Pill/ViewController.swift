3ae7db47e596fabe3efc068fc8304544c7c5fb24
E-Mech <hoezhanhui@gmail.com>
2017-04-01 04:34:52 -0500

    first commit

---
+++ Miracle Pill/Miracle Pill/Miracle Pill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Miracle Pill
+//
+//  Created by Hoe Zhan Hui on 4/1/17.
+//  Copyright © 2017 EMech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
